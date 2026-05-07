25915437c42744b631c9dcbcb44e2d7689b371cc
Jesus Gascon <jesusgascon@gmail.com>
2026-05-07 23:15:35 +0200

    security: Eliminar carpeta de pruebas _scratch del seguimiento de Git

--- _scratch/fix_fichajes.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-let js = fs.readFileSync('app.js', 'utf8');
-
-// Añadiremos la lógica para hacer fetch de los checks de TODOS los empleados.
-// Busco donde se hace el fetchFallback de checks en app.js y lo sustituyo.
